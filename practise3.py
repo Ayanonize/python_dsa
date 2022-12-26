9e8e207cb8f0ac94487cd554f6fd90f621caf4e9
ayan khan <99946200+Ayanonize@users.noreply.github.com>
2022-12-26 21:26:23 +0530

    all code

---
+++ practise3.py
@@ -0,0 +1,16 @@
+class MainClass():
+    def __init__(self):
+        self.data = {
+            "a": "y",
+            "b": "e",
+            "c": "s",
+        }
+
+    def decode(self,_input):
+        finalString = ""
+        for x in _input:
+            finalString += self.data[x]
+        return finalString
+
+main = MainClass()
+print(main.decode("bca"))
